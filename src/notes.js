94ce9d6ac529756d8c921d2fce1008efa278435d
Safitri <ifit.fitri036@yahoo.com>
2021-07-27 11:55:00 +0700

    initial commit

---
+++ src/notes.js
@@ -0,0 +1,13 @@
+const notes = []
+/* ini panduan utk type pengisian data, tidak pengaruh
+    {
+        id: string,
+        title: string,
+        createdAt: string,
+        updatedAt: string,
+        tags: array of string,
+        body: string,
+    }
+*/
+
+module.exports = notes;
